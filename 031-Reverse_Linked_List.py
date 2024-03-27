4a78ce16ddde9256549af1e65f823b232064f32e
Kartik Saxena <mailtokartiksaxena@gmail.com>
2024-03-27 17:47:52 +0530

    Time : 36ms (74.25%) Space : 13.44 MB (93.64%)

---
+++ 031-Reverse_Linked_List.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head):
+        prev_node = None
+        current_node = head
+
+        while current_node:
+            next_node = current_node.next
+            current_node.next = prev_node
+            prev_node = current_node
+            current_node = next_node
+        return prev_node
+
+
+# Time : 36ms (74.25%) Space : 13.44 MB (93.64%)
