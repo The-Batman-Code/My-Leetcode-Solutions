c435464306b598988ab1bb50b4047cf2400f9905
Kartik Saxena <mailtokartiksaxena@gmail.com>
2024-03-27 16:44:43 +0530

    Time : 35ms (77.89%) Space : 12.11 MB (51.79%)

---
+++ 028-Dota2_Senate.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def predictPartyVictory(self, senate):
+        """
+        :type senate: str
+        :rtype: str
+        """
+        senate = list(senate)
+
+        D, R = deque(), deque()
+
+        for i, c in enumerate(senate):
+            if c == "R":
+                R.append(i)
+            else:
+                D.append(i)
+
+        while D and R:
+            dTurn = D.popleft()
+            rTurn = R.popleft()
+
+            if rTurn < dTurn:
+                R.append(rTurn + len(senate))
+            else:
+                D.append(dTurn + len(senate))
+
+        return "Radiant" if R else "Dire"
+
+
+# Time : 35ms (77.89%) Space : 12.11 MB (51.79%)
