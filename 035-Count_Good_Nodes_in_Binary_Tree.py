daf27128cd338f6033081c783ed7b9c5e22ac5bc
Kartik Saxena <mailtokartiksaxena@gmail.com>
2024-03-28 06:06:47 +0530

    Time : 199ms (67.34%) Space : 47.71 MB (8.22%)

---
+++ 035-Count_Good_Nodes_in_Binary_Tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def goodNodes(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+
+        def dfs(root, max_val):
+            if not root:
+                return 0
+
+            res = 1 if root.val >= max_val else 0
+            max_val = max(max_val, root.val)
+
+            res += dfs(root.left, max_val)
+            res += dfs(root.right, max_val)
+            return res
+
+        return dfs(root, root.val)
+
+
+# Time : 199ms (67.34%) Space : 47.71 MB (8.22%)
