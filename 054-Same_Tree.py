7c8639b944b1dc85f1d7fa2e3dccf645161c416d
Kartik Saxena <mailtokartiksaxena@gmail.com>
2024-06-25 11:17:58 +0530

    Time : 36ms (49.25%) Space : 16.57 MB (58.47%)

---
+++ 054-Same_Tree.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+# Time : 36ms (49.25%) Space : 16.57 MB (58.47%)
