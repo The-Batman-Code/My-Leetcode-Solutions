8663ec508782b8a0aaed27ec843a2f4db438e9ca
Kartik Saxena <mailtokartiksaxena@gmail.com>
2024-06-25 11:20:50 +0530

    Time : 40ms (18.59%) Space : 16.53 MB (31.17%)

---
+++ 061-Climbing_Stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one, two = 1, 1
+
+        for i in range(n - 1):
+            temp = one
+            one = one + two
+            two = temp
+
+        return one
+
+
+# Time : 40ms (18.59%) Space : 16.53 MB (31.17%)
