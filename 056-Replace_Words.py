35ffec7134854803c9f2a1f8da9f494312683193
Kartik Saxena <mailtokartiksaxena@gmail.com>
2024-06-25 11:18:51 +0530

    Add files via upload

---
+++ 056-Replace_Words.py
@@ -0,0 +1,12 @@
+# Approach -1
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        sentence = sentence.split()
+        for i in range(len(sentence)):
+            successor = sentence[i]
+            for root in dictionary:
+                if successor.startswith(root):
+                    if len(root) < len(successor):
+                        successor = root
+            sentence[i] = successor
+        return " ".join(sentence)
